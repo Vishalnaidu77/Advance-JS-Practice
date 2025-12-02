9ef3a19c32910f526a302c997123d7c57a90d1d9
Vishal naidu <vminorr@gmail.com>
2025-12-02 20:07:34 +0000

    XHR Object

--- script.js
+++ script.js
@@ -23,30 +23,56 @@
 
          // Callbacks
 
-const createPostBtn = document.querySelector(".create-post")
-const getPostBtn = document.querySelector(".get-post") 
+// const createPostBtn = document.querySelector(".create-post")
+// const getPostBtn = document.querySelector(".get-post") 
 
-const posts = [
-    {title: "Post one", about: "This is post one"},
-    {title: "Post two", about: "This is post two"}
-]
+// const posts = [
+//     {title: "Post one", about: "This is post one"},
+//     {title: "Post two", about: "This is post two"}
+// ]
 
-function createpost(post, callback){
-    setTimeout(() => {
-        posts.push(post);
-        callback()
-    }, 2000);
+// function createpost(post, callback){
+//     setTimeout(() => {
+//         posts.push(post);
+//         callback()
+//     }, 2000);
+// }
+
+// function getPost() {
+//     setTimeout(() => {
+//         posts.forEach(post => {
+//             let div = document.createElement("div")
+//             div.innerHTML = `<strong>${post.title}</strong> : ${post.about}`
+//             document.querySelector(".posts").appendChild(div)
+//         })
+//     }, 1000);
+// }
+
+// getPostBtn.addEventListener("click", getPost)
+// createPostBtn.addEventListener("click", createpost({title: "Post three", about: "This is post three"}, getPost))
+
+
+
+        // AJAX and XHR Object
+
+
+let jokeEl = document.querySelector("#jokes")
+
+function getJoke() {
+    const xhr = new XMLHttpRequest;
+
+    xhr.open('GET', 'https://api.chucknorris.io/jokes/random')
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            
+            jokeEl.innerHTML = JSON.parse(this.response).value
+            
+        }
+    }
+    xhr.send();
 }
 
-function getPost() {
-    setTimeout(() => {
-        posts.forEach(post => {
-            let div = document.createElement("div")
-            div.innerHTML = `<strong>${post.title}</strong> : ${post.about}`
-            document.querySelector(".posts").appendChild(div)
-        })
-    }, 1000);
-}
 
-getPostBtn.addEventListener("click", getPost)
-createPostBtn.addEventListener("click", createpost({title: "Post three", about: "This is post three"}, getPost))
+
+document.querySelector(".get-joke").addEventListener("click", getJoke)
