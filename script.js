de2c1f68265af1f01424a391f41568387112587d
Vishal naidu <vminorr@gmail.com>
2025-12-03 07:18:02 +0000

    Callback Hell

--- script.js
+++ script.js
@@ -56,23 +56,56 @@
         // AJAX and XHR Object
 
 
-let jokeEl = document.querySelector("#jokes")
+// let jokeEl = document.querySelector("#jokes")
 
-function getJoke() {
+// function getJoke() {
+//     const xhr = new XMLHttpRequest;
+
+//     xhr.open('GET', 'https://api.chucknorris.io/jokes/random')
+
+//     xhr.onreadystatechange = function() {
+//         if (this.readyState === 4 && this.status === 200) {
+            
+//             jokeEl.innerHTML = JSON.parse(this.response).value
+            
+//         }
+//     }
+//     xhr.send();
+// }
+
+
+
+// document.querySelector(".get-joke").addEventListener("click", getJoke)
+
+
+
+
+        // Callback Hell
+
+function getData(endpoint, cb) {
     const xhr = new XMLHttpRequest;
 
-    xhr.open('GET', 'https://api.chucknorris.io/jokes/random')
+    xhr.open('GET', endpoint)
 
     xhr.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-            
-            jokeEl.innerHTML = JSON.parse(this.response).value
-            
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+                cb(JSON.parse(this.response));
+            }
         }
     }
-    xhr.send();
+
+    setTimeout(() => {
+        xhr.send();
+    }, Math.floor(Math.random() * 3000) + 1000)
 }
 
-
-
-document.querySelector(".get-joke").addEventListener("click", getJoke)
+getData('./tournament.json', (data) => {
+    console.log(data);
+    getData('./team.json', (data) => {
+        console.log(data);
+        getData('./players.json', (data) => {
+            console.log(data);
+        })
+    })
+})
